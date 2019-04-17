a8cbb07b50d8ccbe6ac9b8b52b089a6eaa6602f2
Strauss Van Wagenen <strauss.vanwagenen@gmail.com>
2019-04-17 12:02:14 -0600

    added git practice and pre course to git hub

---
+++ exercises/Pre Course Project/app.js/app.js
@@ -0,0 +1,16 @@
+
+function myFunction() {
+    var myArray = ['blue', 'red', 'blue', 'red', 'blue', 'purple', 'yellow', 'red'];
+    for (i = 0; i < myArray.length; i++) {
+        if (myArray[i] === 'blue') {
+            console.log('thats blue!')
+        }
+        else { console.log('thats not blue!') }
+
+    }
+
+
+}
+
+
+
